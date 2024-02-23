6faebf82b7521b893cc33d6b96ecbedc9ec77a8c
Adnen-Ben-Abdelaali <benabdelaaliadnen@gmail.com>
2024-02-23 22:32:04 +0000

    auth with keykloak and DockerCompse added

--- api-gateway/src/main/java/net/javaguides/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/net/javaguides/apigateway/ApiGatewayApplication.java
@@ -3,8 +3,11 @@ package net.javaguides.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 //@EnableEurekaClient
 public class ApiGatewayApplication {
 
